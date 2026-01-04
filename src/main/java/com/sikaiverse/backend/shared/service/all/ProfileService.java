637e1ecf6830699a7eff200527905ff468c8171f
Sishir Shrestha <sishershrestha000@gmail.com>
2026-01-04 11:23:54 +0545

    profile page finished and test

--- src/main/java/com/sikaiverse/backend/shared/service/all/ProfileService.java
+++ src/main/java/com/sikaiverse/backend/shared/service/all/ProfileService.java
@@ -1,6 +1,9 @@
 package com.sikaiverse.backend.shared.service.all;
 
+import com.sikaiverse.backend.shared.dto.request.all.UserIdRequest;
 import com.sikaiverse.backend.shared.dto.response.all.ProfileData;
+import com.sikaiverse.backend.shared.entity.all.ProfileEntity;
+import com.sikaiverse.backend.shared.mapper.all.AllEntityToDto;
 import com.sikaiverse.backend.shared.repository.all.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,13 +11,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProfileService {
     private final ProfileRepository profileRepository;
+    private final AllEntityToDto mapper;
 
     @Autowired
-    public ProfileService(ProfileRepository profileRepository){
+    public ProfileService(ProfileRepository profileRepository, AllEntityToDto mapper){
         this.profileRepository = profileRepository;
+        this.mapper = mapper;
     }
 
-    public ProfileData getProfileData(){
+    public ProfileData getProfileData(UserIdRequest request){
 
+        ProfileEntity entity = profileRepository.getUserProfile(request.getUserId());
+        if( entity != null){
+            ProfileData response = mapper.profileMapper(entity);
+            return response;
+        }else{
+            return null;
+        }
     }
 }
